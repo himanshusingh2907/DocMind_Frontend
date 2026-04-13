0c964609a50ffea912eb735e0693264cdc42b4cf
himanshusingh2907 <captaincool6166@gmail.com>
2026-04-13 16:03:54 +0530

    Initial commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosError } from 'axios'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
 
-const API_BASE_URL =  import.meta.env.VITE_API_URL || 'http://localhost:5000'
- //'http://localhost:5000/api'
+const API_BASE_URL = 'http://localhost:5000/api'
+ // 'http://localhost:5000'
 
 type RetryConfig = AxiosRequestConfig & { _retry?: boolean }
 
